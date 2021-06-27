0bb3744f0d4f5b8714af5f1f3a6f963d889e0f75
x-ahmed <appsxapp@gmail.com>
2021-06-27 12:49:40 +0200

    configured client inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+import React from 'react'
+import { render } from 'react-dom'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
+
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        render(<App {...props} />, el)
+    },
+})
